45653666c8ab355157787f33d756ab359c1d5a5a
izzanka <izzancoc.46@gmail.com>
2024-01-24 21:27:35 +0700

    init

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const helmet = require('helmet')
-const morgan = require('morgan')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const app = express()
-const userRoute = require('./routes/users')
-const authRoute = require('./routes/auth')
+const app = express();
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("connected to mongodb")
-}).catch((error) => {
-    console.log("error connecting to mongodb: ", error)
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((error) => {
+    console.log("error connecting to mongodb: ", error);
+  });
 
-app.use(express.json())
-app.use(helmet())
-app.use(morgan("common"))
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-app.use('/api/v1/users', userRoute)
-app.use('/api/v1/auth', authRoute)
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/posts", postRoute);
 
 app.listen(8800, () => {
-    console.log('backend server is running')
-})
+  console.log("backend server is running");
+});
