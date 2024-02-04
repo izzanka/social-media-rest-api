0d387f1830db54b26182bea5c98d912d07a5f709
izzanka <izzancoc.46@gmail.com>
2024-02-04 07:43:02 +0700

    update minor

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,13 +1,14 @@
 import { Post } from "../models/post-model.js";
 import { User } from "../models/user-model.js";
-import {responseError, responseSuccess} from "../helpers/index.js";
-import {response} from "express";
+import { responseError, responseSuccess } from "../helpers/index.js";
 
 export const createPost = async (req, res, next) => {
   try {
     const newPost = new Post(req.body);
     const savedPost = await newPost.save();
-    return res.status(200).json(responseSuccess("create post success", savedPost));
+    return res
+      .status(200)
+      .json(responseSuccess("create post success", savedPost));
   } catch (err) {
     next(err);
   }
@@ -16,11 +17,16 @@ export const createPost = async (req, res, next) => {
 export const updatePost = async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json(responseError("post not found"));
     if (post.userId === req.body.userId) {
       await post.updateOne({ $set: req.body });
-      return res.status(200).json(responseSuccess("post has been updated", null));
+      return res
+        .status(200)
+        .json(responseSuccess("post has been updated", null));
     } else {
-      return res.status(403).json(responseError("you can update only your post"));
+      return res
+        .status(403)
+        .json(responseError("you can update only your post"));
     }
   } catch (err) {
     next(err);
@@ -30,11 +36,16 @@ export const updatePost = async (req, res, next) => {
 export const deletePost = async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json(responseError("post not found"));
     if (post.userId === req.body.userId) {
       await post.deleteOne();
-      return res.status(200).json(responseSuccess("post has been deleted",null));
+      return res
+        .status(200)
+        .json(responseSuccess("post has been deleted", null));
     } else {
-      return res.status(403).json(responseError("you can delete only your post"));
+      return res
+        .status(403)
+        .json(responseError("you can delete only your post"));
     }
   } catch (err) {
     next(err);
@@ -44,7 +55,10 @@ export const deletePost = async (req, res, next) => {
 export const getPostById = async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id);
-    return res.status(200).json(responseSuccess("get post by id success", post));
+    if (!post) return res.status(404).json(responseError("post not found"));
+    return res
+      .status(200)
+      .json(responseSuccess("get post by id success", post));
   } catch (err) {
     next(err);
   }
@@ -53,12 +67,15 @@ export const getPostById = async (req, res, next) => {
 export const likePost = async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id);
+    if (!post) return res.status(404).json(responseError("post not found"));
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
       return res.status(200).json(responseSuccess("post has been liked", null));
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
-      return res.status(200).json(responseSuccess("post has been disliked", null));
+      return res
+        .status(200)
+        .json(responseSuccess("post has been disliked", null));
     }
   } catch (err) {
     next(err);
@@ -68,13 +85,22 @@ export const likePost = async (req, res, next) => {
 export const getPostTimeline = async (req, res, next) => {
   try {
     const currentUser = await User.findById(req.body.userId);
+    if (!currentUser)
+      return res.status(404).json(responseError("current user not found"));
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
         return Post.find({ userId: friendId });
       }),
     );
-    return res.status(200).json(responseSuccess("get post timelines success", userPosts.concat(...friendPosts)));
+    return res
+      .status(200)
+      .json(
+        responseSuccess(
+          "get post timelines success",
+          userPosts.concat(...friendPosts),
+        ),
+      );
   } catch (err) {
     next(err);
   }
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user-model.js";
-import {hash, responseError, responseSuccess} from "../helpers/index.js";
-import {response} from "express";
+import { hash, responseError, responseSuccess } from "../helpers/index.js";
+import { response } from "express";
 
 export const updateUser = async (req, res, next) => {
   try {
@@ -11,9 +11,13 @@ export const updateUser = async (req, res, next) => {
       await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
-      return res.status(200).json(responseSuccess("account has been updated", null));
+      return res
+        .status(200)
+        .json(responseSuccess("account has been updated", null));
     } else {
-      return res.status(403).json(responseError("you can update only your account"));
+      return res
+        .status(403)
+        .json(responseError("you can update only your account"));
     }
   } catch (err) {
     next(err);
@@ -23,7 +27,10 @@ export const updateUser = async (req, res, next) => {
 export const getUserById = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id);
-    return res.status(200).json(responseSuccess("get user by id success", user));
+    if (!user) return res.status(404).json(responseError("user not found"));
+    return res
+      .status(200)
+      .json(responseSuccess("get user by id success", user));
   } catch (err) {
     next(err);
   }
@@ -32,10 +39,15 @@ export const getUserById = async (req, res, next) => {
 export const deleteUser = async (req, res, next) => {
   try {
     if (req.body.userId === req.params.id || req.body.isAdmin) {
-      await User.findByIdAndDelete(req.params.id);
-      return res.status(200).json(responseSuccess("account has been deleted", null));
+      const user = await User.findByIdAndDelete(req.params.id);
+      if (!user) return res.status(404).json(responseError("user not found"));
+      return res
+        .status(200)
+        .json(responseSuccess("account has been deleted", null));
     } else {
-      return res.status(403).json(responseError("you can delete only your account"));
+      return res
+        .status(403)
+        .json(responseError("you can delete only your account"));
     }
   } catch (err) {
     next(err);
@@ -46,14 +58,21 @@ export const followUser = async (req, res, next) => {
   try {
     if (req.body.userId !== req.params.id) {
       const user = await User.findById(req.params.id);
+      if (!user) return res.status(404).json(responseError("user not found"));
       const currentUser = await User.findById(req.body.userId);
+      if (!currentUser)
+        return res.status(404).json(responseError("current user not found"));
       if (!user.followers.includes(req.body.userId)) {
         await user.updateOne({ $push: { followers: req.body.userId } });
         await currentUser.updateOne({ $push: { followings: req.params.id } });
         return res.status(200).json(responseSuccess("user has been followed"));
       } else {
-        return res.status(403).json(responseError("you already follow this user"));
+        return res
+          .status(403)
+          .json(responseError("you already follow this user"));
       }
+    } else {
+      return res.status(400).json(responseError("user id cannot be same"));
     }
   } catch (err) {
     next(err);
@@ -64,14 +83,21 @@ export const unfollowUser = async (req, res, next) => {
   try {
     if (req.body.userId !== req.params.id) {
       const user = await User.findById(req.params.id);
+      if (!user) return res.status(404).json(responseError("user not found"));
       const currentUser = await User.findById(req.body.userId);
+      if (!currentUser)
+        return res.status(404).json(responseError("current user not found"));
       if (user.followers.includes(req.body.userId)) {
         await user.updateOne({ $pull: { followers: req.body.userId } });
         await currentUser.updateOne({ $pull: { followings: req.params.id } });
-        return res.status(200).json(responseSuccess("user has been unfollowed", null));
+        return res
+          .status(200)
+          .json(responseSuccess("user has been unfollowed", null));
       } else {
         return res.status(403).json(responseError("you dont follow this user"));
       }
+    } else {
+      return res.status(400).json(responseError("user id cannot be same"));
     }
   } catch (err) {
     next(err);
